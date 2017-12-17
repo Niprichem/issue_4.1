b2c8c0c9c378987b9e1a6a10d50201c316bd6962
Niprichem <vbolshakoff@yandex.ru>
2017-12-18 02:27:53 +0600

    new

--- homework_4.1.py
+++ homework_4.1.py
@@ -2,21 +2,34 @@ import os
 import pandas as pd
 
 
-def count_top3(path, years):
-    cols =['name', 'gender', 'count']
+def top_count_names(path, years, top_count):
+    cols =['Name', 'Gender', 'Count']
     data_list = [pd.read_csv(os.path.join(path, 'yob{}.txt'.format(year)), names=cols)
         for year in years]
-    data = data_list[0]
-    for d in data_list[1:]:
-        data.merge(d, on='name')
-    return data.sort_values(by='count', ascending=False)[:3]
+    pd_data = pd.concat(data_list).groupby('Name').sum()
+    top_pd_data = pd_data.sort_values(by='Count', ascending=False).head(top_count)
+    return top_pd_data
+
+
+def gender_count_dynamics(path, years):
+    cols =['Name', 'Gender', 'Count']
+    data = {year: pd.read_csv(os.path.join(path, 'yob{}.txt'.format(year)), names=cols)
+        for year in years}
+    pd_data = pd.concat(data, names=['Year'])
+    pd_data_gender = pd_data.groupby(['Year', 'Gender']).sum()
+    return pd_data_gender
 
 
 def main():
     path = 'names'
+
     all_years = ((1880,), (1900, 1950, 2000))
+    top_count = 3
     for years in all_years:
-        print(count_top3(path, years))
+        print(top_count_names(path, years, top_count))
+
+    years = (1900, 1950, 2000)
+    print(gender_count_dynamics(path, years))
 
 
 if __name__ == '__main__':
